20a1ec936816b96fe1e9be1f089917c42ed6b5b5
Alexandre <alexandreribeiro@procobservicos.com.br>
2021-03-24 10:51:06 -0300

    first commit

---
+++ js/owl/setup.js
@@ -0,0 +1,33 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('.owl-carousel3').owlCarousel3({
+    loop:false,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
